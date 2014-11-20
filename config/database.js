9cb2e2b96cb5bbbe5f1ab46ce2434f688e31e316
EC2 Default User <ec2-user@ip-172-31-56-220.ec2.internal>
2014-11-20 03:53:39 +0000

    forever

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'localhost:27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'ec2-54-173-242-62.compute-1.amazonaws.com:27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
 
